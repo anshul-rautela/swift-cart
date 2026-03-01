a1544973707e12332678d17ba40e69f1fc020438
Anshul Rautela <anshulrautela123456789@gmail.com>
2026-03-01 21:20:14 +0530

    Update CORS config

--- backend/src/main/java/com/anshul/e_commerce/config/CorsConfig.java
+++ backend/src/main/java/com/anshul/e_commerce/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOrigins("https://swift-cart-qy1f.vercel.app", "http://localhost:3000")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
