297c5b81914ba6d6efd80f26c618436b3f55106a
Anshul Rautela <anshulrautela123456789@gmail.com>
2026-03-01 16:20:43 +0530

    add global cors config

---
+++ backend/src/main/java/com/anshul/e_commerce/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.anshul.e_commerce.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000", "https://swift-cart-qy1f.vercel.app")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
