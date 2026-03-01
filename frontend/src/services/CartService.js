81decd1be3e857952b931c140109beb034260074
Anshul Rautela <anshulrautela123456789@gmail.com>
2026-03-01 15:40:52 +0530

    fix api urls for production

--- frontend/src/services/CartService.js
+++ frontend/src/services/CartService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = "http://localhost:8080/cart";
+const API_URL = `${process.env.REACT_APP_API_URL}/cart`;
 
 export const getCartItems = (userId) => {
     return axios.get(`${API_URL}/${userId}`);
--- frontend/src/services/ProductService.js
+++ frontend/src/services/ProductService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = "http://localhost:8080/products";
+const API_URL = `${process.env.REACT_APP_API_URL}/products`;
 
 export const getAllProducts = () => {
     return axios.get(API_URL);
