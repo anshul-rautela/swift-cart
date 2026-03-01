ff2946fad3b1150363568a7e58bcc8682c2ba19f
Anshul Rautela <anshulrautela123456789@gmail.com>
2026-03-01 16:28:27 +0530

    allow all origins test

--- backend/src/main/java/com/anshul/e_commerce/config/CorsConfig.java
+++ backend/src/main/java/com/anshul/e_commerce/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "https://swift-cart-qy1f.vercel.app")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
